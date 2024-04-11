c2441324618207ab464bc1a1fb4a57b1555819d8
pat955 <patricijap8@gmail.com>
2024-04-11 11:12:41 +0200

    added config struct

--- main.go
+++ main.go
@@ -18,10 +18,18 @@ func main() {
 		fmt.Scanln(&input)
 		x := commands[input]
 		x.Command()
+		x.Config.Command()
 	}
 }
 
 type cliCommand struct {
+	Name    string
+	Desc    string
+	Command func() error
+	Config  *Config
+}
+
+type Config struct {
 	Name    string
 	Desc    string
 	Command func() error
@@ -30,13 +38,14 @@ type cliCommand struct {
 func getCommands() map[string]cliCommand {
 	return map[string]cliCommand{
 		"help": {
-			Name: "help",
+			Name: "Help",
 			Desc: "Get info about this cli and other commands",
 			Command: func() error {
 				fmt.Println("help received")
 				return nil
 			},
 		},
+
 		"exit": {
 			Name: "Exit",
 			Desc: "Exit command line",
@@ -46,5 +55,28 @@ func getCommands() map[string]cliCommand {
 				return nil
 			},
 		},
+
+		"map": {
+			Name: "Map",
+			Desc: "Map the next 20 area of pokemon",
+			Command: func() error {
+				call("")
+				return nil
+			},
+			Config: &Config{
+				Name: "Previous",
+				Desc: "Get the previous 20 map areas",
+				Command: func() error {
+					fmt.Println("previous!")
+					return nil
+				},
+			},
+		},
 	}
+}
+
+func call(s string) {
+	endpoint := fmt.Sprintf("https://pokeapi.co/api/v2/%s/", s)
+	fmt.Println(endpoint)
+
 }
