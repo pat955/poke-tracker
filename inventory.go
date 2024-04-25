adf1e42d5ef4792466f6418a696f3dbc22eab15c
pat955 <patricijap8@gmail.com>
2024-04-25 11:48:18 +0200

    json item struct

--- inventory.go
+++ inventory.go
@@ -77,6 +77,66 @@ type ItemInventory struct {
 }
 
 type Item struct {
-	Name string
-	Desc string
+	Attributes []struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"attributes"`
+	BabyTriggerFor any `json:"baby_trigger_for"`
+	Category       struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"category"`
+	Cost          int `json:"cost"`
+	EffectEntries []struct {
+		Effect   string `json:"effect"`
+		Language struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"language"`
+		ShortEffect string `json:"short_effect"`
+	} `json:"effect_entries"`
+	FlavorTextEntries []struct {
+		Language struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"language"`
+		Text         string `json:"text"`
+		VersionGroup struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"version_group"`
+	} `json:"flavor_text_entries"`
+	FlingEffect any `json:"fling_effect"`
+	FlingPower  any `json:"fling_power"`
+	GameIndices []struct {
+		GameIndex  int `json:"game_index"`
+		Generation struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"generation"`
+	} `json:"game_indices"`
+	HeldByPokemon []any  `json:"held_by_pokemon"`
+	ID            int    `json:"id"`
+	Machines      []any  `json:"machines"`
+	Name          string `json:"name"`
+	Names         []struct {
+		Language struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"language"`
+		Name string `json:"name"`
+	} `json:"names"`
+	Sprites struct {
+		Default string `json:"default"`
+	} `json:"sprites"`
+}
+
+func (i *Item) GetID() int {
+	return i.ID
+}
+func (i *Item) GetURL() string {
+	return i.Category.URL
+}
+func (i *Item) PrintInfo() {
+	fmt.Println("Nothing here, printing out item info")
 }
